d57f82a3f210af53129ec9868e4095a0ea407b32
Arpine Mkoyan <arpinemkoyan01@gmail.com>
2022-04-21 23:11:49 +0400

    homework2

---
+++ homework2/script.js
@@ -0,0 +1,67 @@
+class myPromise {
+
+    constructor(callbackFn) {
+        this.handleError = () => { };
+        this.handleSuccess = () => { };
+        this.resolveFn = this.resolveFn.bind(this);
+        this.rejectFn = this.rejectFn.bind(this);
+
+        callbackFn(this.resolveFn, this.rejectFn);
+    }
+
+    then(handleSuccess) {
+        this.handleSuccess = handleSuccess;
+        return this;
+    }
+
+    catch(handleError) {
+        this.handleError = handleError;
+        return this;
+    }
+
+    resolveFn(value) {
+        try {
+            this.handleSuccess(value);
+        } catch (error) {
+            this.rejectFn(error);
+        }
+    }
+
+    rejectFn(error) {
+        this.handleError(error);
+    }
+}
+
+
+const myAjaxMethod = (url, config) => {
+
+    return new myPromise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(config.method, url, true);
+
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                resolve(xhr.response);
+            }
+            else {
+                reject(Error(xhr.statusText));
+            }
+        };
+
+        xhr.onerror = function () {
+            reject(Error("Network Error"));
+        };
+        xhr.send()
+    });
+
+};
+
+myAjaxMethod("https://corona-api.com/countries", {
+    method: 'GET',
+    headers: {}
+})
+    .then((user) => {
+        console.log(user);
+    }).catch((error) => {
+        console.log(error);
+    });
