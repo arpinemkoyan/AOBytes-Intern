ae86f27508c096b826d1eb3212838eb86bf6f992
Arpine Mkoyan <arpinemkoyan01@gmail.com>
2022-04-29 03:49:45 +0400

    homework4

---
+++ homework4/homeworks/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Container from './Container';
+import React from 'react';
+
+
+function App() {
+  return (<div className='content'>
+    <Container />
+  </div>
+  );
+}
+
+export default App;
